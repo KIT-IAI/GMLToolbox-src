b75eac1ffa07fdfb0dab3f90219dcf55391e4e94
Franz-Josef Kaiser <github@fjkaiser.net>
2020-07-03 08:21:36 +0200

    Fix layout of info dialog

--- Info.h
+++ Info.h
@@ -84,166 +84,163 @@ using namespace System::Drawing;
 		/// </summary>
 		void InitializeComponent(void)
 		{
-      System::ComponentModel::ComponentResourceManager^  resources = ( gcnew System::ComponentModel::ComponentResourceManager( Info::typeid ) );
-      this->pictureBoxXPlanungLogo = ( gcnew System::Windows::Forms::PictureBox() );
-      this->pictureBoxFZKLogo = ( gcnew System::Windows::Forms::PictureBox() );
-      this->label1 = ( gcnew System::Windows::Forms::Label() );
-      this->label2 = ( gcnew System::Windows::Forms::Label() );
-      this->textBoxHaftungsAusschluss = ( gcnew System::Windows::Forms::TextBox() );
-      this->button1 = ( gcnew System::Windows::Forms::Button() );
-      this->textBoxKontakt = ( gcnew System::Windows::Forms::TextBox() );
-      this->label3 = ( gcnew System::Windows::Forms::Label() );
-      this->label4 = ( gcnew System::Windows::Forms::Label() );
-      this->label5 = ( gcnew System::Windows::Forms::Label() );
-      this->groupBox1 = ( gcnew System::Windows::Forms::GroupBox() );
-      ( cli::safe_cast<System::ComponentModel::ISupportInitialize^>( this->pictureBoxXPlanungLogo ) )->BeginInit();
-      ( cli::safe_cast<System::ComponentModel::ISupportInitialize^>( this->pictureBoxFZKLogo ) )->BeginInit();
+      System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Info::typeid));
+      this->pictureBoxXPlanungLogo = (gcnew System::Windows::Forms::PictureBox());
+      this->pictureBoxFZKLogo = (gcnew System::Windows::Forms::PictureBox());
+      this->label1 = (gcnew System::Windows::Forms::Label());
+      this->label2 = (gcnew System::Windows::Forms::Label());
+      this->textBoxHaftungsAusschluss = (gcnew System::Windows::Forms::TextBox());
+      this->button1 = (gcnew System::Windows::Forms::Button());
+      this->textBoxKontakt = (gcnew System::Windows::Forms::TextBox());
+      this->label3 = (gcnew System::Windows::Forms::Label());
+      this->label4 = (gcnew System::Windows::Forms::Label());
+      this->label5 = (gcnew System::Windows::Forms::Label());
+      this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxXPlanungLogo))->BeginInit();
+      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxFZKLogo))->BeginInit();
       this->groupBox1->SuspendLayout();
       this->SuspendLayout();
       // 
       // pictureBoxXPlanungLogo
       // 
-      this->pictureBoxXPlanungLogo->BackgroundImage = ( cli::safe_cast<System::Drawing::Image^>( resources->GetObject( L"pictureBoxXPlanungLogo.BackgroundImage" ) ) );
-      this->pictureBoxXPlanungLogo->ErrorImage = ( cli::safe_cast<System::Drawing::Image^>( resources->GetObject( L"pictureBoxXPlanungLogo.ErrorImage" ) ) );
-      this->pictureBoxXPlanungLogo->Image = ( cli::safe_cast<System::Drawing::Image^>( resources->GetObject( L"pictureBoxXPlanungLogo.Image" ) ) );
-      this->pictureBoxXPlanungLogo->InitialImage = ( cli::safe_cast<System::Drawing::Image^>( resources->GetObject( L"pictureBoxXPlanungLogo.InitialImage" ) ) );
-      this->pictureBoxXPlanungLogo->Location = System::Drawing::Point( 8, 12 );
+      this->pictureBoxXPlanungLogo->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBoxXPlanungLogo.Image")));
+      this->pictureBoxXPlanungLogo->Location = System::Drawing::Point(12, 12);
       this->pictureBoxXPlanungLogo->Name = L"pictureBoxXPlanungLogo";
-      this->pictureBoxXPlanungLogo->Size = System::Drawing::Size( 351, 117 );
+      this->pictureBoxXPlanungLogo->Size = System::Drawing::Size(350, 120);
       this->pictureBoxXPlanungLogo->SizeMode = System::Windows::Forms::PictureBoxSizeMode::CenterImage;
       this->pictureBoxXPlanungLogo->TabIndex = 0;
       this->pictureBoxXPlanungLogo->TabStop = false;
       // 
       // pictureBoxFZKLogo
       // 
-      this->pictureBoxFZKLogo->Image = ( cli::safe_cast<System::Drawing::Image^>( resources->GetObject( L"pictureBoxFZKLogo.Image" ) ) );
-      this->pictureBoxFZKLogo->Location = System::Drawing::Point( 258, 253 );
+      this->pictureBoxFZKLogo->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBoxFZKLogo.Image")));
+      this->pictureBoxFZKLogo->Location = System::Drawing::Point(258, 253);
       this->pictureBoxFZKLogo->Name = L"pictureBoxFZKLogo";
-      this->pictureBoxFZKLogo->Size = System::Drawing::Size( 97, 87 );
+      this->pictureBoxFZKLogo->Size = System::Drawing::Size(97, 87);
       this->pictureBoxFZKLogo->SizeMode = System::Windows::Forms::PictureBoxSizeMode::CenterImage;
       this->pictureBoxFZKLogo->TabIndex = 1;
       this->pictureBoxFZKLogo->TabStop = false;
       // 
       // label1
       // 
-      this->label1->Font = ( gcnew System::Drawing::Font( L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-        static_cast<System::Byte>( 0 ) ) );
-      this->label1->Location = System::Drawing::Point( 6, 21 );
+      this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+        static_cast<System::Byte>(0)));
+      this->label1->Location = System::Drawing::Point(6, 21);
       this->label1->Name = L"label1";
-      this->label1->Size = System::Drawing::Size( 146, 16 );
+      this->label1->Size = System::Drawing::Size(146, 16);
       this->label1->TabIndex = 2;
       this->label1->Text = L"GML-Toolbox ";
       // 
       // label2
       // 
-      this->label2->Font = ( gcnew System::Drawing::Font( L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-        static_cast<System::Byte>( 0 ) ) );
-      this->label2->Location = System::Drawing::Point( 8, 49 );
+      this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+        static_cast<System::Byte>(0)));
+      this->label2->Location = System::Drawing::Point(8, 49);
       this->label2->Name = L"label2";
-      this->label2->Size = System::Drawing::Size( 146, 16 );
+      this->label2->Size = System::Drawing::Size(146, 16);
       this->label2->TabIndex = 3;
       this->label2->Text = L"Version 11.1";
       // 
       // textBoxHaftungsAusschluss
       // 
-      this->textBoxHaftungsAusschluss->Location = System::Drawing::Point( 8, 370 );
+      this->textBoxHaftungsAusschluss->Location = System::Drawing::Point(12, 366);
       this->textBoxHaftungsAusschluss->Multiline = true;
       this->textBoxHaftungsAusschluss->Name = L"textBoxHaftungsAusschluss";
       this->textBoxHaftungsAusschluss->ReadOnly = true;
       this->textBoxHaftungsAusschluss->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
-      this->textBoxHaftungsAusschluss->Size = System::Drawing::Size( 347, 170 );
+      this->textBoxHaftungsAusschluss->Size = System::Drawing::Size(350, 170);
       this->textBoxHaftungsAusschluss->TabIndex = 4;
       // 
       // button1
       // 
       this->button1->DialogResult = System::Windows::Forms::DialogResult::Cancel;
-      this->button1->Location = System::Drawing::Point( 145, 557 );
+      this->button1->Location = System::Drawing::Point(150, 548);
       this->button1->Name = L"button1";
-      this->button1->Size = System::Drawing::Size( 75, 23 );
+      this->button1->Size = System::Drawing::Size(75, 23);
       this->button1->TabIndex = 6;
       this->button1->Text = L"Ok";
       // 
       // textBoxKontakt
       // 
       this->textBoxKontakt->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-      this->textBoxKontakt->Font = ( gcnew System::Drawing::Font( L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-        static_cast<System::Byte>( 0 ) ) );
-      this->textBoxKontakt->ForeColor = System::Drawing::Color::FromArgb( static_cast<System::Int32>( static_cast<System::Byte>( 0 ) ), static_cast<System::Int32>( static_cast<System::Byte>( 0 ) ),
-        static_cast<System::Int32>( static_cast<System::Byte>( 192 ) ) );
-      this->textBoxKontakt->Location = System::Drawing::Point( 8, 243 );
+      this->textBoxKontakt->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+        static_cast<System::Byte>(0)));
+      this->textBoxKontakt->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+        static_cast<System::Int32>(static_cast<System::Byte>(192)));
+      this->textBoxKontakt->Location = System::Drawing::Point(12, 248);
       this->textBoxKontakt->Multiline = true;
       this->textBoxKontakt->Name = L"textBoxKontakt";
       this->textBoxKontakt->ReadOnly = true;
-      this->textBoxKontakt->Size = System::Drawing::Size( 347, 106 );
+      this->textBoxKontakt->Size = System::Drawing::Size(350, 106);
       this->textBoxKontakt->TabIndex = 7;
       // 
       // label3
       // 
-      this->label3->Font = ( gcnew System::Drawing::Font( L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-        static_cast<System::Byte>( 0 ) ) );
+      this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+        static_cast<System::Byte>(0)));
       this->label3->ForeColor = System::Drawing::Color::Blue;
-      this->label3->Location = System::Drawing::Point( 161, 23 );
+      this->label3->Location = System::Drawing::Point(161, 23);
       this->label3->Name = L"label3";
-      this->label3->Size = System::Drawing::Size( 154, 23 );
+      this->label3->Size = System::Drawing::Size(154, 23);
       this->label3->TabIndex = 8;
       this->label3->Text = L"Unterstützte Standards";
       // 
       // label4
       // 
-      this->label4->Font = ( gcnew System::Drawing::Font( L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-        static_cast<System::Byte>( 0 ) ) );
-      this->label4->Location = System::Drawing::Point( 8, 73 );
+      this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+        static_cast<System::Byte>(0)));
+      this->label4->Location = System::Drawing::Point(8, 73);
       this->label4->Name = L"label4";
-      this->label4->Size = System::Drawing::Size( 112, 16 );
+      this->label4->Size = System::Drawing::Size(112, 16);
       this->label4->TabIndex = 9;
       this->label4->Text = L"17. Juni 2020";
       // 
       // label5
       // 
-      this->label5->Font = ( gcnew System::Drawing::Font( L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-        static_cast<System::Byte>( 0 ) ) );
+      this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+        static_cast<System::Byte>(0)));
       this->label5->ForeColor = System::Drawing::SystemColors::ActiveCaption;
-      this->label5->Location = System::Drawing::Point( 161, 49 );
+      this->label5->Location = System::Drawing::Point(161, 49);
       this->label5->Name = L"label5";
-      this->label5->Size = System::Drawing::Size( 169, 49 );
+      this->label5->Size = System::Drawing::Size(169, 49);
       this->label5->TabIndex = 10;
       this->label5->Text = L"XPlanung,  INSPIRE, CityGML, CityGML-ADE, ALKIS/NAS, BoreholeML";
-      this->label5->Click += gcnew System::EventHandler( this, &Info::label5_Click );
+      this->label5->Click += gcnew System::EventHandler(this, &Info::label5_Click);
       // 
       // groupBox1
       // 
-      this->groupBox1->Controls->Add( this->label4 );
-      this->groupBox1->Controls->Add( this->label3 );
-      this->groupBox1->Controls->Add( this->label5 );
-      this->groupBox1->Controls->Add( this->label1 );
-      this->groupBox1->Controls->Add( this->label2 );
-      this->groupBox1->Location = System::Drawing::Point( 8, 135 );
+      this->groupBox1->Controls->Add(this->label4);
+      this->groupBox1->Controls->Add(this->label3);
+      this->groupBox1->Controls->Add(this->label5);
+      this->groupBox1->Controls->Add(this->label1);
+      this->groupBox1->Controls->Add(this->label2);
+      this->groupBox1->Location = System::Drawing::Point(12, 136);
       this->groupBox1->Name = L"groupBox1";
-      this->groupBox1->Size = System::Drawing::Size( 347, 100 );
+      this->groupBox1->Size = System::Drawing::Size(350, 100);
       this->groupBox1->TabIndex = 12;
       this->groupBox1->TabStop = false;
       // 
       // Info
       // 
       this->AcceptButton = this->button1;
-      this->AutoScaleBaseSize = System::Drawing::Size( 5, 13 );
+      this->AutoScaleBaseSize = System::Drawing::Size(5, 13);
       this->CancelButton = this->button1;
-      this->ClientSize = System::Drawing::Size( 387, 599 );
-      this->Controls->Add( this->button1 );
-      this->Controls->Add( this->textBoxHaftungsAusschluss );
-      this->Controls->Add( this->pictureBoxFZKLogo );
-      this->Controls->Add( this->pictureBoxXPlanungLogo );
-      this->Controls->Add( this->groupBox1 );
-      this->Controls->Add( this->textBoxKontakt );
-      this->Icon = ( cli::safe_cast<System::Drawing::Icon^>( resources->GetObject( L"$this.Icon" ) ) );
+      this->ClientSize = System::Drawing::Size(374, 581);
+      this->Controls->Add(this->button1);
+      this->Controls->Add(this->textBoxHaftungsAusschluss);
+      this->Controls->Add(this->pictureBoxFZKLogo);
+      this->Controls->Add(this->pictureBoxXPlanungLogo);
+      this->Controls->Add(this->groupBox1);
+      this->Controls->Add(this->textBoxKontakt);
+      this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
       this->MaximizeBox = false;
       this->MinimizeBox = false;
       this->Name = L"Info";
       this->Text = L"Info über GML Toolbox";
-      ( cli::safe_cast<System::ComponentModel::ISupportInitialize^>( this->pictureBoxXPlanungLogo ) )->EndInit();
-      ( cli::safe_cast<System::ComponentModel::ISupportInitialize^>( this->pictureBoxFZKLogo ) )->EndInit();
-      this->groupBox1->ResumeLayout( false );
-      this->ResumeLayout( false );
+      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxXPlanungLogo))->EndInit();
+      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxFZKLogo))->EndInit();
+      this->groupBox1->ResumeLayout(false);
+      this->ResumeLayout(false);
       this->PerformLayout();
 
     }
