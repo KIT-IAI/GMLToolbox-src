2d59726c4541b94a2f988d742fb96d48009b9883
Franz-Josef Kaiser <franz-josef.kaiser@partner.kit.edu>
2023-11-02 11:20:51 +0100

    Catch exception when loading plugin

--- PluginLoader.h
+++ PluginLoader.h
@@ -5,68 +5,78 @@ using namespace System;
 using namespace System::Collections::Generic;
 using namespace System::IO;
 using namespace System::Reflection;
+using namespace System::Windows::Forms;
 using namespace KIT::GMLToolbox::Plugin;
 
 namespace KIT {
-  namespace GMLToolbox {
-    namespace Plugin {
-      public ref class PluginLoader
-      {
-      public:
-        static Type^ CheckAssembly(String^ assemblyFile)
-        {
-          Type^ theResult = nullptr;
-          Type^ theType = IGMLToolboxCommand::typeid;
-          auto assemblyName = gcnew AssemblyName();
-          assemblyName->CodeBase = assemblyFile;
-          auto appDomain = AppDomain::CreateDomain("PluginReflectionDomain");
-          auto assembly = appDomain->Load(assemblyName);
-          auto types = assembly->GetTypes();
-          for each (auto type in types)
-          {
-            if (theType->IsAssignableFrom(type) && type != theType)
+    namespace GMLToolbox {
+        namespace Plugin {
+            public ref class PluginLoader
             {
-              theResult = type;
-              break;
-            }
-          }
-          AppDomain::Unload(appDomain);
+            public:
+                static Type^ CheckAssembly(String^ assemblyFile)
+                {
+                    Type^ theResult = nullptr;
+                    try
+                    {
+                        Type^ theType = IGMLToolboxCommand::typeid;
+                        auto assemblyName = gcnew AssemblyName();
+                        assemblyName->CodeBase = assemblyFile;
+                        auto appDomain = AppDomain::CreateDomain("PluginReflectionDomain");
+                        auto assembly = appDomain->Load(assemblyName);
+                        auto types = assembly->GetTypes();
+                        for each (auto type in types)
+                        {
+                            if (theType->IsAssignableFrom(type) && type != theType)
+                            {
+                                theResult = type;
+                                break;
+                            }
+                        }
+                        AppDomain::Unload(appDomain);
+                    }
+                    catch (Exception^ ex)
+                    {
+                        auto str = String::Format("The plugin file {0} could not be loaded. The plugin is ignored this time."
+                            + "\r\n\r\n{1}", assemblyFile, ex->Message);
+                        MessageBox::Show(str, "Plugin Loader Information", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
+                    }
 
-          return theResult;
+                    return theResult;
+                }
+
+            public:
+                static IGMLToolboxCommand^ LoadAssembly(String^ assemblyFile)
+                {
+                    IGMLToolboxCommand^ theInstance = nullptr;
+                    auto theType = CheckAssembly(assemblyFile);
+                    if (theType)
+                    {
+                        auto assembly = Assembly::LoadFrom(assemblyFile);
+                        auto theInst = assembly->CreateInstance(theType->FullName);
+                        theInstance = dynamic_cast<IGMLToolboxCommand^>(theInst);
+                    }
+
+                    return theInstance;
+                }
+
+            public:
+                static List<IGMLToolboxCommand^>^ LoadPlugins()
+                {
+                    auto results = gcnew List<IGMLToolboxCommand^>();
+                    auto appPath = AppDomain::CurrentDomain->BaseDirectory;
+                    auto files = Directory::EnumerateFiles(Path::GetDirectoryName(appPath), "*.dll");
+                    for each (auto file in files)
+                    {
+                        auto inst = LoadAssembly(file);
+                        if (inst)
+                        {
+                            results->Add(inst);
+                        }
+                    }
+                    return results;
+                }
+            };
         }
-
-      public:
-        static IGMLToolboxCommand^ LoadAssembly(String^ assemblyFile)
-        {
-          IGMLToolboxCommand^ theInstance = nullptr;
-          auto theType = CheckAssembly(assemblyFile);
-          if (theType)
-          {
-            auto assembly = Assembly::LoadFrom(assemblyFile);
-            auto theInst = assembly->CreateInstance(theType->FullName);
-            theInstance = dynamic_cast<IGMLToolboxCommand^>(theInst);
-          }
-
-          return theInstance;
-        }
-
-      public:
-        static List<IGMLToolboxCommand^>^ LoadPlugins()
-        {
-          auto results = gcnew List<IGMLToolboxCommand^>();
-          auto appPath = AppDomain::CurrentDomain->BaseDirectory;
-          auto files = Directory::EnumerateFiles(Path::GetDirectoryName(appPath), "*.dll");
-          for each (auto file in files)
-          {
-            auto inst = LoadAssembly(file);
-            if (inst)
-            {
-              results->Add(inst);
-            }
-          }
-          return results;
-        }
-      };
     }
-  }
 }
